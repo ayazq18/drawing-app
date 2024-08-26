df33bd7bbf86d21e7d270ae1edf2ccd75a8ff609
Ayaz Shahnawaz Qureshi <131868270+ayazq18@users.noreply.github.com>
2024-08-26 14:45:54 +0530

    enquebar added

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -4,6 +4,8 @@ import { auth } from './fireBase';
 import { Box, Button, TextField, Typography, Alert, Stack, IconButton, Container } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { Icon } from '@iconify/react';
+import { useSnackbar } from 'notistack';
+
 
 const Register = () => {
     const [email, setEmail] = useState('');
@@ -11,6 +13,8 @@ const Register = () => {
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
+    const { enqueueSnackbar } = useSnackbar();
+
     const navigate = useNavigate()
     const handleRegister = async (e) => {
         e.preventDefault();
@@ -19,84 +23,86 @@ const Register = () => {
 
         if (password !== confirmPassword) {
             setError("Passwords do not match");
+            enqueueSnackbar('Passwords do not match!', { variant: 'error' });
             return;
         }
 
         try {
             await createUserWithEmailAndPassword(auth, email, password);
             setSuccess("Registration successful! You can now log in.");
+            enqueueSnackbar('Registration successful! You can now log in.', { variant: 'success' });
         } catch (err) {
             setError(err.message);
         }
     };
 
     return (
-    <Container maxWidth="xs">
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                mt: 8,
-                p: 2,
-                borderRadius: 1,
-                boxShadow: 3,
-                bgcolor: 'background.paper',
-            }}
-        >
-            <Stack direction='row' alignItems='center' gap={2} mb={2}>
-                <IconButton sx={{ width: '40px', color: 'white' }}>
-                    <Icon icon="vscode-icons:file-type-excalidraw" />
-                </IconButton>
-                <Typography variant="h5" sx={{ }}>Register</Typography>
-            </Stack>
-            {error && <Alert severity="error" sx={{ mb: 2 }}>{error}</Alert>}
-            {success && <Alert severity="success" sx={{ mb: 2 }}>{success}</Alert>}
-            <form onSubmit={handleRegister} style={{ width: '100%' }}>
-                <TextField
-                    label="Email"
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    fullWidth
-                    required
-                    sx={{ mb: 2 }}
-                />
-                <TextField
-                    label="Password"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    fullWidth
-                    required
-                    sx={{ mb: 2 }}
-                />
-                <TextField
-                    label="Confirm Password"
-                    type="password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    fullWidth
-                    required
-                    sx={{ mb: 2 }}
-                />
-                <Stack direction='row' gap={2}>
-                    <Button type="submit" variant="contained" color="primary" fullWidth>
-                        Register
-                    </Button>
-                    <Button
-                        type="submit"
-                        variant="outlined"
-                        color="primary"
-                        fullWidth
-                        onClick={() => navigate('/login')}
-
-                    >
-                        Login
-                    </Button>
+        <Container maxWidth="xs">
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    mt: 8,
+                    p: 2,
+                    borderRadius: 1,
+                    boxShadow: 3,
+                    bgcolor: 'background.paper',
+                }}
+            >
+                <Stack direction='row' alignItems='center' gap={2} mb={2}>
+                    <IconButton sx={{ width: '40px', color: 'white' }}>
+                        <Icon icon="vscode-icons:file-type-excalidraw" />
+                    </IconButton>
+                    <Typography variant="h5" sx={{}}>Register</Typography>
                 </Stack>
-            </form>
-        </Box>
+                {error && <Alert severity="error" sx={{ mb: 2 }}>{error}</Alert>}
+                {success && <Alert severity="success" sx={{ mb: 2 }}>{success}</Alert>}
+                <form onSubmit={handleRegister} style={{ width: '100%' }}>
+                    <TextField
+                        label="Email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        fullWidth
+                        required
+                        sx={{ mb: 2 }}
+                    />
+                    <TextField
+                        label="Password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        fullWidth
+                        required
+                        sx={{ mb: 2 }}
+                    />
+                    <TextField
+                        label="Confirm Password"
+                        type="password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        fullWidth
+                        required
+                        sx={{ mb: 2 }}
+                    />
+                    <Stack direction='row' gap={2}>
+                        <Button type="submit" variant="contained" color="primary" fullWidth>
+                            Register
+                        </Button>
+                        <Button
+                            type="submit"
+                            variant="outlined"
+                            color="primary"
+                            fullWidth
+                            onClick={() => navigate('/login')}
+
+                        >
+                            Login
+                        </Button>
+                    </Stack>
+                </form>
+            </Box>
         </Container>
     );
 };
